1dab09b3a7704af0521bb47e7d3678a5bf221802
BLaurenB <20262199+BLaurenB@users.noreply.github.com>
2018-05-30 16:04:23 -0600

    adding js challenge and js bst

---
+++ binary_search_tree/bst.js
@@ -0,0 +1,22 @@
+// My attempts at a Binary Search Tree
+// I think that adding and inserting are the same function, so I'll just do that.
+
+
+export default class Bst {
+  constructor() {
+    this.root = null
+  }
+
+  insert(num) {
+    // is there a root yet?
+
+    // there is a root. Compare the passed num to the left and right.
+      // if left === null
+      // if right === null
+      // if n < left
+      // if n > right
+      // if left < num < right
+  }
+
+
+}
---
+++ binary_search_tree/node.js
@@ -0,0 +1,10 @@
+
+class Node {
+  constructor( num, left = null, right = null) {
+    this.data = num
+    this.left = left
+    this.right = right
+  }
+}
+
+module.exports = Node
