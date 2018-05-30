fc92a760dad4015e29e6acf003537ff0899782ba
BLaurenB <20262199+BLaurenB@users.noreply.github.com>
2018-05-30 16:18:25 -0600

    node class tests

---
+++ binary_search_tree/bst.js
@@ -0,0 +1,24 @@
+// My attempts at a Binary Search Tree
+// I think that adding and inserting are the same function, so I'll just do that.
+
+
+class Bst {
+  constructor() {
+    this.root = null
+  }
+
+  insert(num) {
+    // is there a root yet?
+
+    // there is a root. Compare the passed num to the left and right.
+      // if left === null
+      // if right === null
+      // if n < left
+      // if n > right
+      // if left < num < right
+  }
+
+
+}
+
+module.exports = Bst
---
+++ binary_search_tree/node.js
@@ -0,0 +1,9 @@
+class Node {
+  constructor( num, left = null, right = null) {
+    this.data = num
+    this.left = left
+    this.right = right
+  }
+}
+
+module.exports = Node
